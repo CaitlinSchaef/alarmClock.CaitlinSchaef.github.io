2348d070a6ed4a486d6c72b8acfc6e9ec1303125
Caitlin Schaeffer <160537700+CaitlinSchaef@users.noreply.github.com>
2024-03-29 15:12:16 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
 // Tip: We will be using the Date object and setInterval and/or setTimeout
+
+//clock functions
+
+// date functions, do you need month and day and year or just first day?
+
+// alarm function
